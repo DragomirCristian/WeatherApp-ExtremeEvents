b02293f3d9a5b4d2a7f7228f413fa62017b36e79
BotezMihai <mihai.botezz@gmail.com>
2019-08-22 14:41:53 +0300

    https call

--- src/main/java/com/dragomircristian/extremeevents/controllers/ExtremeEventsController.java
+++ src/main/java/com/dragomircristian/extremeevents/controllers/ExtremeEventsController.java
@@ -12,16 +12,38 @@ import com.google.auth.Credentials;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.*;
 
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContexts;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+//import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.SSLContext;
 import java.io.*;
+import java.security.GeneralSecurityException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/extreme-events")
@@ -29,7 +51,7 @@ public class ExtremeEventsController {
 
     @Autowired
     ExtremeEventsService extremeEventsService;
-
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(ExtremeEventsController.class);
     @RequestMapping(value = "/city/{city}", params = {"p", "s"}, method = RequestMethod.GET)
     public ResponseEntity<Page<ExtremeEvent>> findAllByCity(@PathVariable("city") String city, @RequestParam("p") int pageNumber, @RequestParam("s") int pageSize) throws InvalidPageNumberException, InvalidPageSizeException {
         if (pageNumber < -0)
@@ -99,12 +121,64 @@ public class ExtremeEventsController {
             return "Error";
         }
     }
-
+    @Autowired
+    private RestTemplate restTemplate;
     @RequestMapping(value = "/upload-extreme-event", method = RequestMethod.POST)
-    public ResponseEntity<?> upload(@RequestHeader(value = "Authorization") String access_token, @RequestBody String token) {
-        System.out.println(access_token);
-        RestTemplate restTemplate= new RestTemplate();
-        ResponseEntity<?> responseEntity=restTemplate.getForObject("localhost")
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<?> upload(@RequestHeader(value = "Authorization") String access_token) throws GeneralSecurityException {
+       // System.out.println(access_token);
+       // RestTemplate restTemplate= new RestTemplate();
+       // ResponseEntity responseEntity=restTemplate.postForEntity("https://localhost:8445/check-token",access_token,String.class);
+       // LOGGER.info((String)responseEntity.getBody());
+
+//        TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
+//        SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+//        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext,
+//                NoopHostnameVerifier.INSTANCE);
+//
+//        Registry<ConnectionSocketFactory> socketFactoryRegistry =
+//                RegistryBuilder.<ConnectionSocketFactory> create()
+//                        .register("https", sslsf)
+//                        .register("http", new PlainConnectionSocketFactory())
+//                        .build();
+//
+//        BasicHttpClientConnectionManager connectionManager =
+//                new BasicHttpClientConnectionManager(socketFactoryRegistry);
+//        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf)
+//                .setConnectionManager(connectionManager).build();
+//        String urlOverHttps="https://localhost:8445/check-token";
+//        HttpComponentsClientHttpRequestFactory requestFactory =
+//                new HttpComponentsClientHttpRequestFactory(httpClient);
+//        RestTemplate restTemplate=new RestTemplate(requestFactory);
+//     //   ResponseEntity responseEntity=restTemplate.postForEntity("https://localhost:8445/check-token",access_token,String.class);
+//        ResponseEntity<String> response =restTemplate
+//                .exchange(urlOverHttps, HttpMethod.GET, null, String.class);
+
+
+        ResponseEntity responseEntity=restTemplate.postForEntity("https://localhost:8445/check-token",access_token,String.class);
+        LOGGER.info("test");
+        return new ResponseEntity<>("test", HttpStatus.OK);
+    }
+
+    @Bean
+    public RestTemplate restTemplate()
+            throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
+                .loadTrustMaterial(null, acceptingTrustStrategy)
+                .build();
+
+        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(csf)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory =
+                new HttpComponentsClientHttpRequestFactory();
+
+        requestFactory.setHttpClient(httpClient);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+        return restTemplate;
     }
 }
